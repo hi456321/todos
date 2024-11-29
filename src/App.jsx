f6f6f5ad570002eb07747c2457566e1aa83abf0d
Your Name <you@example.com>
2024-11-30 00:28:58 +0900

    컴포넌트 기능 분리

--- src/App.jsx
+++ src/App.jsx
@@ -1,45 +1,16 @@
 import React, {useState} from "react";
 import SearchData from "./component/SearchData";
 import DataList from "./component/DataList";
-import axiosModule from "./utils/axios/axios";
-
-const url = process.env.REACT_APP_DATA_API_URL;
-const serviceKey = process.env.REACT_APP_DATA_API_KEY;
 
 function App() {
-  const [inputs, setInputs] = useState({
-    page: '1',
-    record: '10'
-  });
-  const {page, record} = inputs;
-  const onChange = e => {
-    const {name, value} = e.target;
-    setInputs({
-      ...inputs,
-      [name]: value
-    });
-  };
 
   const [dataArr, setDataArr] = useState([]);
-
-  const onSearch = async () => {
-
-    const response = await axiosModule({ type: 'get', url, params: { serviceKey, pageIndex: page, recordCountPerPage: record }});
-
-      if(response !== null) {
-        //console.log(response.data.response.body.items);
-        // setState
-        const items = response.data.response.body.items;
-        setDataArr(items);
-      }
-
-  };
 
   return (
     <>
       <h1>전국어린이놀이시설우수시설정보서비스</h1>
       <br />
-      <SearchData page={page} record={record} onChange={onChange} onSearch={onSearch} />
+      <SearchData setDataArr={setDataArr} />
       <br />
       <DataList dataArr={dataArr} />
     </>
--- src/component/SearchData.jsx
+++ src/component/SearchData.jsx
@@ -1,11 +1,39 @@
-import React from 'react';
+import React, {useState} from 'react';
+import axiosModule from "../utils/axios/axios";
 
-function SearchData({page, record, onChange, onSearch}) {
+const url = process.env.REACT_APP_DATA_API_URL;
+const serviceKey = process.env.REACT_APP_DATA_API_KEY;
+
+const SearchData = ({setDataArr}) => {
+
+    const [inputs, setInputs] = useState({
+        page: '1',
+        record: '10'
+      });
+
+    const onChange = e => {
+        const {name, value} = e.target;
+        setInputs({
+          ...inputs,
+          [name]: value
+        });
+      };
+
+      const onSearch = async () => {
+
+        const response = await axiosModule({ type: 'get', url, params: { serviceKey, pageIndex: inputs.page, recordCountPerPage: inputs.record }});
+    
+          if(response !== null) {
+            const items = response.data.response.body.items;
+            setDataArr(items);
+          }
+    
+      };
 
     return (
         <div>
-            <input name='page' placeholder='페이지' value={page} onChange={onChange} />
-            <input name='record' placeholder='한 페이지 결과 수' value={record} onChange={onChange} />
+            <input name='page' placeholder='페이지' value={inputs.page} onChange={onChange} />
+            <input name='record' placeholder='한 페이지 결과 수' value={inputs.record} onChange={onChange} />
             <button onClick={onSearch}>조회</button>
         </div>
     );
