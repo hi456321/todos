19730bbb63b4efe426d3bf364b2cfbc754ccc457
jsy <wjdtjddyd135@naver.com>
2024-11-30 23:10:27 +0900

    타이틀, 날짜 다듬기

--- src/component/YoutubeList.jsx
+++ src/component/YoutubeList.jsx
@@ -1,21 +1,31 @@
 import React from "react";
 
 function YoutubeItem({youtubeItem, key}) {
-    console.log(youtubeItem)
-    console.log(youtubeItem)
+    // console.log(youtubeItem)
+
     const imageStyle = {
         width: '100%',
         height: '300px'
     };
+
+    const sliceTitle = title => {
+        if(title.length > 30) {
+            return title.slice(0, 30) + '....';
+        }
+    }
+
+    const sliceDate = date => {
+        return date.slice(0, 10);
+    }
 
     return (
         <div className="col">
             <div className="card shadow-sm">
                 <img className="imageStyle" src={youtubeItem.snippet.thumbnails.high.url} style={imageStyle} />
                 <div className="card-body">
-                    <h5 className="card-text">{youtubeItem.snippet.title}</h5>
+                    <h5 className="card-text">{sliceTitle(youtubeItem.snippet.title)}</h5>
                     <div className="d-flex justify-content-between align-items-center">
-                    <small className="text-muted">{youtubeItem.snippet.publishedAt}</small>
+                    <small className="text-muted">{sliceDate(youtubeItem.snippet.publishedAt)}</small>
                     </div>
                 </div>
             </div>
