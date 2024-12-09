7bc95cb4e1200f51ccf118b91947aa4d499dec38
Your Name <you@example.com>
2024-12-09 21:45:35 +0900

    add

--- 2-types-projects-quiz/loading.ts
+++ 2-types-projects-quiz/loading.ts
@@ -20,6 +20,13 @@
 
   type ResourceLoadState = LoadingState | SuccessState | FailState;
 
+  const printLoginState = (resourceLoadState: ResourceLoadState) => {
+    switch(resourceLoadState.state) {
+      case 'loading': console.log('👀 loading...'); break;
+      case 'success': console.log(`😃 ${resourceLoadState.response.body}`); break;
+      case 'fail':  console.log(`😱 ${resourceLoadState.reason}`); break;
+    }
+  }
   printLoginState({ state: 'loading' }); // 👀 loading...
   printLoginState({ state: 'success', response: { body: 'loaded' } }); // 😃 loaded
   printLoginState({ state: 'fail', reason: 'no network' }); // 😱 no network
